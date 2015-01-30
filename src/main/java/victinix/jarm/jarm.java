39833554dccce5ed37866a3ad844794f5110139d
VictiniX888 <victiniy@gmail.com>
2015-01-30 20:10:31 +0800

    Basic mod file

---
+++ src/main/java/victinix/jarm/jarm.java
@@ -0,0 +1,31 @@
+package victinix.jarm;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import victinix.jarm.lib.Data;
+
+/**
+ * Created by VictiniX888 on 30-Jan-2015.
+ */
+
+@Mod(modid = Data.MODID, name = Data.MODNAME, version = Data.VERSION)
+
+public class jarm {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/victinix/jarm/lib/Data.java
@@ -0,0 +1,12 @@
+package victinix.jarm.lib;
+
+/**
+ * Created by VictiniX888 on 30-Jan-2015.
+ */
+
+public class Data {
+
+    public static final String MODID = "jarm";
+    public static final String MODNAME = "Just Another Random Mod";
+    public static final String VERSION = "0.1";
+}
