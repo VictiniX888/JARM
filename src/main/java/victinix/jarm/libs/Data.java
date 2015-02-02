b356046f412ffbc7cb75960c55527d48c97868d2
VictiniX888 <victiniy@gmail.com>
2015-02-02 17:35:58 +0800

    Build 6

--- src/main/java/victinix/jarm/libs/Data.java
+++ src/main/java/victinix/jarm/libs/Data.java
@@ -15,5 +15,5 @@ public class Data {
 
     public static final String MODID = "jarm";
     public static final String MODNAME = "Just Another Random Mod";
-    public static final String VERSION = "0.1.5";
+    public static final String VERSION = "0.1.6";
 }
