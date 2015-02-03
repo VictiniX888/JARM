4a09548907eb9c802eb7b945aa90e9a411596bf6
VictiniX888 <victiniy@gmail.com>
2015-02-03 11:20:57 +0800

    Emerald Sword recipe

--- src/main/java/victinix/jarm/libs/Recipes.java
+++ src/main/java/victinix/jarm/libs/Recipes.java
@@ -12,6 +12,21 @@ public class Recipes {
 
     public static void registerCraftingRecipes() {
 
+        if(Configurations.hotdogRegister)
+            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(modItems.hotdog, Configurations.hotdogCraftingAmount, 0), new Object[]{
+                    new ItemStack(Items.bread),
+                    new ItemStack(Items.cooked_chicken)
+            }));
+
+        if(Configurations.emeraldSwordRegister)
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(modItems.emeraldSword, 1, 0), new Object[]{
+                    "A",
+                    "A",
+                    "B",
+                    'A', "gemEmerald",
+                    'B', "stickWood"
+            }));
+
         if(Configurations.randomGunRegister)
             GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(modItems.randomGun, 1, 0), new Object[] {
                     "AAA",
@@ -19,12 +34,6 @@ public class Recipes {
                     "  A",
                     'A', "ingotIron",
                     'B', new ItemStack(Blocks.stone_button)
-            }));
-
-        if(Configurations.hotdogRegister)
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(modItems.hotdog, Configurations.hotdogCraftingAmount, 0), new Object[]{
-                    new ItemStack(Items.bread),
-                    new ItemStack(Items.cooked_chicken)
             }));
     }
 }
