5203f79f9024566a9c9578caf1b93493b1f6a30e
VictiniX888 <victiniy@gmail.com>
2015-02-09 18:28:39 +0800

    Emerald Sword has Fire Aspect

--- src/main/java/victinix/jarm/items/randomWeapons/emeraldSword.java
+++ src/main/java/victinix/jarm/items/randomWeapons/emeraldSword.java
@@ -1,7 +1,10 @@
 package victinix.jarm.items.randomWeapons;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.potion.PotionEffect;
 import victinix.jarm.libs.Configurations;
 import victinix.jarm.libs.Data;
 import victinix.jarm.libs.Tabs;
@@ -18,5 +21,13 @@ public class emeraldSword extends ItemSword {
         setCreativeTab(Tabs.tabRandom);
         if(Configurations.emeraldSwordRegister)
             GameRegistry.registerItem(this, name);
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack itemStack, EntityLivingBase entityAttacked, EntityLivingBase entityAttacking) {
+
+        entityAttacked.setFire(4);
+        
+        return true;
     }
 }
