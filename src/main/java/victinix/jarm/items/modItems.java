df1e17515f65efc3658542a5bafb158ec5493fe1
VictiniX888 <victiniy@gmail.com>
2015-02-09 18:12:49 +0800

    Remove Magic Carpet

--- src/main/java/victinix/jarm/items/modItems.java
+++ src/main/java/victinix/jarm/items/modItems.java
@@ -3,7 +3,6 @@ package victinix.jarm.items;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.util.EnumHelper;
 import victinix.jarm.items.food.hotdog;
-import victinix.jarm.items.magicStuff.magicCarpet;
 import victinix.jarm.items.randomWeapons.emeraldSword;
 import victinix.jarm.items.randomWeapons.randomGun;
 import victinix.jarm.libs.Configurations;
@@ -14,8 +13,6 @@ public class modItems {
 
     public static Item hotdog;
 
-    public static Item magicCarpet;
-
     public static Item emeraldSword;
     public static Item randomGun;
 
@@ -24,8 +21,6 @@ public class modItems {
         RANDOMEMERALD = EnumHelper.addToolMaterial("RANDOMEMERALD", Configurations.emeraldSwordHarvestLevel, Configurations.emeraldSwordDurability, (float) Configurations.emeraldSwordMiningSpeed, (float) Configurations.emeraldSwordDamage, Configurations.emeraldSwordEnchantability);
 
         hotdog = new hotdog(Configurations.hotdogHeal, (float) Configurations.hotdogSaturation, Configurations.hotdogWolfMeat);
-
-        magicCarpet = new magicCarpet();
 
         emeraldSword = new emeraldSword(modItems.RANDOMEMERALD);
         randomGun = new randomGun();
