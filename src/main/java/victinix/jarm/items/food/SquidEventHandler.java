5e4b5ef5795674ea363e9230190c48799eeccb3e
VictiniX888 <victiniy@gmail.com>
2015-03-14 17:46:53 +0800

    Squids drop Cool Squid

---
+++ src/main/java/victinix/jarm/items/food/SquidEventHandler.java
@@ -0,0 +1,20 @@
+package victinix.jarm.items.food;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import victinix.jarm.items.ModItems;
+
+public class SquidEventHandler {
+
+    @SubscribeEvent
+    public void squidEvent(LivingDropsEvent event) {
+
+        if(event.entity instanceof EntitySquid) {
+            ItemStack itemStack = new ItemStack(ModItems.coolSquid, 2);
+            event.drops.add(new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, itemStack));
+        }
+    }
+}
