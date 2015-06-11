8a55e19c7c619cdc8b7e9a6519a91619e6cfac75
VictiniX888 <victiniy@gmail.com>
2015-06-11 09:12:51 +0800

    Fix Demonic Slate > Ethereal Slate

--- src/main/java/victinix/jarm/compat/BotaniaCompat.java
+++ src/main/java/victinix/jarm/compat/BotaniaCompat.java
@@ -13,6 +13,6 @@ public class BotaniaCompat {
         BotaniaAPI.registerManaInfusionRecipe(new ItemStack(GameRegistry.findItem("AWWayofTime", "reinforcedSlate")), new ItemStack(GameRegistry.findItem("AWWayofTime", "blankSlate")), 2000);
         BotaniaAPI.registerManaInfusionRecipe(new ItemStack(GameRegistry.findItem("AWWayofTime", "imbuedSlate")), new ItemStack(GameRegistry.findItem("AWWayofTime", "reinforcedSlate")), 5000);
         BotaniaAPI.registerManaInfusionRecipe(new ItemStack(GameRegistry.findItem("AWWayofTime", "demonicSlate")), new ItemStack(GameRegistry.findItem("AWWayofTime", "imbuedSlate")), 15000);
-        BotaniaAPI.registerManaInfusionRecipe(new ItemStack(GameRegistry.findItem("AWWayofTime", "etherealSlate")), new ItemStack(GameRegistry.findItem("AWWayofTime", "demonicSlate")), 30000);
+        BotaniaAPI.registerManaInfusionRecipe(new ItemStack(GameRegistry.findItem("AWWayofTime", "bloodMagicBaseItems"), 1, 27), new ItemStack(GameRegistry.findItem("AWWayofTime", "demonicSlate")), 30000);
     }
 }
