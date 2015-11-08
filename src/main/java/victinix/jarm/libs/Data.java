07686e27c60d589ada4de5f41d011799f160a1c5
VictiniX888 <victiniy@gmail.com>
2015-11-08 15:42:52 +0800

    v1.1.0

--- src/main/java/victinix/jarm/libs/Data.java
+++ src/main/java/victinix/jarm/libs/Data.java
@@ -4,6 +4,6 @@ public class Data {
 
     public static final String MODID = "jarm";
     public static final String MODNAME = "Just Another Random Mod";
-    public static final String VERSION = "1.0.10";
+    public static final String VERSION = "1.1.0";
     public static final String DEPENDENCIES = "after:harvestcraft";
 }
