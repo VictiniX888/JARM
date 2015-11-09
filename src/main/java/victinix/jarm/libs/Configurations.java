878003c032ac9bb4dc4eda062290d1a5a50f546f
VictiniX888 <victiniy@gmail.com>
2015-11-09 08:48:09 +0800

    Change default amount of dust from ore to be 12 via configs & update description of some other configs

--- src/main/java/victinix/jarm/libs/Configurations.java
+++ src/main/java/victinix/jarm/libs/Configurations.java
@@ -35,6 +35,7 @@ public class Configurations {
     public static float hotdogSaturation;
 
     public static String[] canCrush;
+    public static int crushDustAmount;
     public static String[] dittoBlacklist;
     public static boolean xProducerDamage;
 
@@ -83,11 +84,12 @@ public class Configurations {
 
         canCrush = config.getStringList("canCrush", category, new String[] {
                 "minecraft:cobblestone", "minecraft:iron_ore", "minecraft:gold_ore", "minecraft:coal_ore", "minecraft:diamond_ore", "minecraft:emerald_ore", "minecraft:lapis_ore", "minecraft:redstone_ore", "minecraft:lit_redstone_ore"
-        }, "Placeholder description");
+        }, "Things the Crushing Wand can crush");
+        crushDustAmount = config.get(category, "crushDustAmount", 12, "Amount of dust that drops when you use the Crushing Wand on an ore").getInt();
         dittoBlacklist = config.getStringList("dittoBlacklist", category, new String[] {
                 "tile.chest", "item.dolly.normal.full"
-        }, "Placeholder description");
-        xProducerDamage = config.get(category, "xProducerDamage", true).getBoolean(true);
+        }, "Things the Ditto cannot dupe");
+        xProducerDamage = config.get(category, "xProducerDamage", true, "Whether the XProducer damages the player when creating XP").getBoolean(true);
 
         if(config.hasChanged()) {
             config.save();
