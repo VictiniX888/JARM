7de5d7611f6bf123f27609d1f5d29e16b913c3f7
VictiniX888 <victiniy@gmail.com>
2016-04-23 18:01:29 +0800

    JARM 3.0.1

--- src/main/java/victinix/jarm/lib/Data.java
+++ src/main/java/victinix/jarm/lib/Data.java
@@ -4,5 +4,5 @@ public class Data {
 
     public static final String MODID = "jarm";
     public static final String NAME = "Just Another Random Mod";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.0.1";
 }
